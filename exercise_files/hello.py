b34dd58fddfde317186e491c1f2d0ab94e864f07
anastasia <anastasia.bobere@gmail.com>
2025-02-09 13:31:47 +0200

    3d part

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -1,3 +1,4 @@
-
-# Print "hello, world!" to the terminal
-print('Hello, World!')
+from decimal import Decimal, getcontext
+getcontext().prec = 2  # Set precision to 6 decimal places
+result = Decimal('1') / Decimal('7')
+print(result)
